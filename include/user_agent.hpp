2aa82010d6c1322c90a02ea8b34915ffb7d99a1b
jonathonl <jlefaive@gmail.com>
2016-01-26 17:21:13 -0500

    Fixed connect issues in client.

--- include/user_agent.hpp
+++ include/user_agent.hpp
@@ -19,13 +19,29 @@ namespace manifold
       endpoint() {}
       endpoint(const uri& uri)
         : host_(uri.host()), port_(uri.port()), encrypted_(uri.scheme_name() == "https")
-      { }
-      endpoint(const std::string& host, bool encrypted = true, unsigned short port = 0)
+      {
+        if (!port_)
+          port_ = (unsigned short)(encrypted_ ? 443 : 80);
+      }
+      endpoint(const std::string& host, bool encrypted = false, unsigned short port = 0)
         : host_(host), port_(port), encrypted_(encrypted)
-      { }
+      {
+        if (!port_)
+          port_ = (unsigned short)(encrypted_ ? 443 : 80);
+      }
+      bool operator<(const endpoint& other) const
+      {
+        return (this->host_ < other.host_ || this->port_ < other.port_ || (int)this->encrypted_ < (int)other.encrypted_);
+      }
       const std::string& host() const { return host_; }
       unsigned short port() const { return port_; }
       bool encrypted() const { return encrypted_; }
+      std::string socket_address() const
+      {
+        std::stringstream ret;
+        ret << this->host_ << ":" << this->port_;
+        return ret.str();
+      }
     private:
       std::string host_;
       unsigned short port_;
@@ -45,13 +61,9 @@ namespace manifold
 
       void make_request(const endpoint& ep, const std::function<void(const std::error_code& connect_error, client::request&& req)>& cb)
       {
-        unsigned short port = ep.port();
-        if (!port)
-          port = (unsigned short)(ep.encrypted() ? 443 : 80);
+        std::string socket_address = ep.host() + ":" + std::to_string(ep.port());
 
-        std::string socket_address = ep.host() + ":" + std::to_string(port);
-
-        auto it = this->sessions_.find(socket_address);
+        auto it = this->sessions_.find(ep);
         if (it != this->sessions_.end())
         {
           if (it->second.conn)
@@ -68,6 +80,7 @@ namespace manifold
         {
           session& sess = this->sessions_[socket_address];
           sess.pending_requests.emplace(cb);
+          sess.ep = ep;
 
 
           if (ep.encrypted())
@@ -77,6 +90,8 @@ namespace manifold
           else
           {
             sess.sock = std::make_shared<manifold::non_tls_socket>(this->io_service_);
+            non_tls_socket s(this->io_service_);
+            sess.test.emplace(std::move(s));
 
             this->tcp_resolver_.async_resolve(asio::ip::tcp::resolver::query(ep.host(), std::to_string(ep.port())), [this, socket_address](const std::error_code& ec, asio::ip::tcp::resolver::iterator it)
             {
@@ -92,7 +107,8 @@ namespace manifold
                   std::cout << it->host_name() << std::endl;
                   std::cout << it->endpoint().address().to_string() << std::endl;
                   std::cout << it->endpoint().port() << std::endl;
-                  ((asio::ip::tcp::socket&)*sess_it->second.sock).async_connect(*it, [this, socket_address](const std::error_code& ec)
+                  non_tls_socket& sock = dynamic_cast<non_tls_socket&>(*sess_it->second.sock);
+                  ((asio::ip::tcp::socket&)sock).async_connect(*it, [this, socket_address](const std::error_code& ec)
                   {
                     auto sess_it = this->sessions_.find(socket_address);
                     if (sess_it != this->sessions_.end())
@@ -104,7 +120,7 @@ namespace manifold
                       else
                       {
                         sess_it->second.conn = std::make_shared<v1_connection<request_head, response_head>>(std::move(dynamic_cast<non_tls_socket&>(*sess_it->second.sock)));
-                        sess_it->second.conn->on_close(std::bind(&user_agent::handle_connection_close, this, std::placeholders::_1, sess_it->second.socket_address));
+                        sess_it->second.conn->on_close(std::bind(&user_agent::handle_connection_close, this, std::placeholders::_1, sess_it->second.ep.socket_address()));
                         sess_it->second.conn->run();
                         sess_it->second.process_pending_requests(std::error_code());
                       }
@@ -121,11 +137,11 @@ namespace manifold
       class session
       {
       public:
-        std::string socket_address;
+        endpoint ep;
         std::shared_ptr<http::connection<request_head, response_head>> conn;
         std::shared_ptr<socket> sock;
         std::queue<std::function<void(const std::error_code& connect_error, client::request&& req)>> pending_requests;
-        std::queue<socket> test;
+        std::queue<non_tls_socket> test;
 
         void process_pending_requests(const std::error_code& ec)
         {
@@ -143,7 +159,7 @@ namespace manifold
                 else
                 {
                   std::uint32_t stream_id = this->conn->create_stream(0, 0);
-                  this->pending_requests.front()(std::error_code(), client::request(request_head("/", "GET", {{"user-agent", "Manifold"}}), this->conn, stream_id, this->socket_address));
+                  this->pending_requests.front()(std::error_code(), client::request(request_head("/", "GET", {{"user-agent", "Manifold"}}), this->conn, stream_id, this->ep.socket_address()));
                 }
               }
               this->pending_requests.pop();
@@ -156,7 +172,7 @@ namespace manifold
 
       asio::io_service& io_service_;
       asio::ip::tcp::resolver tcp_resolver_;
-      std::map<std::string, session> sessions_;
+      std::map<endpoint, session> sessions_;
 
       void handle_connection_close(errc ec, const std::string& sock_addr)
       {
