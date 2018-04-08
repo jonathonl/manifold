c6766f89e10640b81579d2af681e627dd81c09f6
jonathonl <jonathonl@users.noreply.github.com>
2018-04-08 16:53:53 -0400

    Changes license to MPL 2.0.

--- include/manifold/http_entity_transfer_client.hpp
+++ include/manifold/http_entity_transfer_client.hpp
@@ -1,3 +1,7 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 #pragma once
 #ifndef MANIFOLD_HTTP_STREAM_CLIENT_HPP
 #define MANIFOLD_HTTP_STREAM_CLIENT_HPP
